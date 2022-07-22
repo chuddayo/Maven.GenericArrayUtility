a899cc71785a71c2ea8aa2cf4c058fd053829ba4
Ryan Chudd <ryan.a.chudd@gmail.com>
2022-07-22 19:02:36 -0400

    generic array utility methods

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -1,9 +1,6 @@
 package com.zipcodewilmington.arrayutility;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by leon on 3/6/18.
@@ -17,19 +14,37 @@ public class ArrayUtility<E> {
 
     public Integer countDuplicatesInMerge(E[] arrayToMerge, E valueToEvaluate) {
         array.addAll(new ArrayList<>(Arrays.asList(arrayToMerge)));
+        return getNumberOfOccurrences(valueToEvaluate);
+    }
+    public Integer getNumberOfOccurrences(E valueToEvaluate) {
         Integer counter = 0;
         for (E e : array) {
             if(e.equals(valueToEvaluate)) counter++;
         }
         return counter;
     }
-    public E getNumberOfOccurrences(E valueToEvaluate) {
-        return null;
-    }
     public E getMostCommonFromMerge(E[] arrayToMerge) {
-        return null;
+        array.addAll(new ArrayList<>(Arrays.asList(arrayToMerge)));
+        HashMap<E, Integer> frequencyMap = new HashMap<>();
+        for (E e : array) {
+            if (frequencyMap.containsKey(e)) {
+                frequencyMap.put(e, frequencyMap.get(e) + 1);
+            } else {
+                frequencyMap.put(e, 1);
+            }
+        }
+
+        Map.Entry<E, Integer> maxEntry = Collections.max(frequencyMap.entrySet(), Map.Entry.comparingByValue());
+        return maxEntry.getKey();
     }
     public E[] removeValue(E valueToRemove) {
-        return null;
+        array.removeIf(i -> Objects.equals(i, valueToRemove));
+        if (valueToRemove instanceof Integer) {
+            return (E[]) array.toArray(new Integer[0]);
+        } else if (valueToRemove instanceof String) {
+            return (E[]) array.toArray(new String[0]);
+        } else {
+            return (E[]) array.toArray(new Long[0]);
+        }
     }
 }
